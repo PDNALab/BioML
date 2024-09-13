9161deb34cbe6e2a07fc2e3f4cddc6c60eab265f
Jokent Gaza <jokent.gaza@login9.ufhpc>
2024-09-13 14:55:37 -0400

    Modified analysis.py

--- scripts/1_post-design/analysis.py
+++ scripts/1_post-design/analysis.py
@@ -2,6 +2,7 @@ import mdtraj as md
 import os
 import numpy as np
 import glob
+import re
 
 pep1_rmsd = []
 
@@ -55,10 +56,31 @@ for motif in monomer_motifs:
         f.write("{0}\n".format(rmsd[0]))
 
 
+pep1_iptm = []
+
+with open('ipTM.dat', 'w') as f:
+    f.write("ipTM values of complex\n")
+
+# Iterate over each motif
+for i in range(1, len(monomer_motifs)+1):
+    filepath = f"complex/{i}-motif_*/output_nomsa/log.txt"
+    
+    for filename in glob.glob(filepath):
+        with open(filename, 'r') as file:
+            for line in file:
+                if "rank_001_" in line:
+                    fields = line.split()
+                    if len(fields) >= 6:
+                        match = re.search(r'ipTM=([^ ]+)', fields[5])
+                        if match:
+                            pep1_iptm.append(float(match.group(1)))
+                            with open('ipTM.dat', 'a') as f:
+                                f.write("{0}\n".format(match.group(1)))
+
 with open('passed.dat', 'w') as f:
     f.write('List of successful proteins\n')
 
-for i, (rog, rmsd) in enumerate(zip(pep1_rog, pep1_rmsd)):
-    if rog < 1.4 and rmsd < 0.2 :
+for i, (rog, rmsd, iptm) in enumerate(zip(pep1_rog, pep1_rmsd, pep1_iptm)):
+    if rog < 1.4 and rmsd < 0.2 and iptm > 7.0:
         with open('passed.dat', 'a') as f:
             f.write(monomer_motifs[i] + '\n')
