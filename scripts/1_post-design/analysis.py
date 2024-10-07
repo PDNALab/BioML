46f703ef2f417219d0439fb3e4e92b5b856e24da
Jokent Gaza <jokent.gaza@c1007a-s35.ufhpc>
2024-10-07 18:01:05 -0400

    Added scritps for linker design

--- scripts/1_post-design/analysis.py
+++ scripts/1_post-design/analysis.py
@@ -10,17 +10,17 @@ monomer_motifs = [dir for dir in os.listdir("monomer") if '-motif' in dir]
 monomer_motifs.sort(key=lambda dir: int(dir.split('-')[0]))
 
 with open('rmsd.dat', 'w') as f:
-    f.write("RMSD of monomer vs in-complex\n")
+    f.write("RMSD of monomer vs full CAR\n")
 
 # Iterate over each motif
 for motif in monomer_motifs:
-    complex_dir = [dir for dir in os.listdir("complex") if dir == motif][0]
+    complex_dir = [dir for dir in os.listdir("full") if dir == motif][0]
 
-    complex_pep=glob.glob(f"complex/{complex_dir}/output_nomsa/seq_custom_unrelaxed_rank_001_*.pdb")
+    complex_pep=glob.glob(f"full/{complex_dir}/output_nomsa/seq_custom_unrelaxed_rank_001_*.pdb")
     monomer_pep=glob.glob(f"monomer/{motif}/output_nomsa/seq_custom_unrelaxed_rank_001_*.pdb")
 
     traj1 = md.load(complex_pep[0])
-    chainC = traj1.topology.select('chainid 2')
+    chainC = traj1.topology.select('resid 21 to 100')
 
     traj2 = md.load(monomer_pep[0])
     chainA = traj2.topology.select('chainid 0')
@@ -55,32 +55,10 @@ for motif in monomer_motifs:
     with open('rog.dat', 'a') as f:
         f.write("{0}\n".format(rmsd[0]))
 
-
-pep1_iptm = []
-
-with open('ipTM.dat', 'w') as f:
-    f.write("ipTM values of complex\n")
-
-# Iterate over each motif
-for i in range(1, len(monomer_motifs)+1):
-    filepath = f"complex/{i}-motif_*/output_nomsa/log.txt"
-    
-    for filename in glob.glob(filepath):
-        with open(filename, 'r') as file:
-            for line in file:
-                if "rank_001_" in line:
-                    fields = line.split()
-                    if len(fields) >= 6:
-                        match = re.search(r'ipTM=([^ ]+)', fields[5])
-                        if match:
-                            pep1_iptm.append(float(match.group(1)))
-                            with open('ipTM.dat', 'a') as f:
-                                f.write("{0}\n".format(match.group(1)))
-
 with open('passed.dat', 'w') as f:
     f.write('List of successful proteins\n')
 
-for i, (rog, rmsd, iptm) in enumerate(zip(pep1_rog, pep1_rmsd, pep1_iptm)):
-    if rog < 1.4 and rmsd < 0.2 and iptm > 7.0:
+for i, (rog, rmsd) in enumerate(zip(pep1_rog, pep1_rmsd)):
+    if rog < 1.4 and rmsd < 0.2:
         with open('passed.dat', 'a') as f:
             f.write(monomer_motifs[i] + '\n')
--- scripts/1_post-design/run.sh
+++ scripts/1_post-design/run.sh
@@ -7,14 +7,14 @@ scripts_dir=$(pwd)
 cd $scripts_dir\/../../submissions/$motif
 seq_dir=$(pwd)
 echo "Working directory: $seq_dir"
-mkdir sanity; mkdir sanity/monomer; mkdir sanity/complex;
+mkdir sanity; mkdir sanity/monomer; mkdir sanity/complex; mkdir sanity/full
 
 # Get list of successful designs (pAE < 10)
 awk '
-$5 > 85 {
+$3 < 15 {
     match($9, /motif_[0-9]+_/)
     if (RSTART > 0) {
-        print $5 "\t" substr($9, RSTART, RLENGTH)
+        print $3 "\t" substr($9, RSTART, RLENGTH)
     }
 }' af2/designed_score.sc | \
 sort -n | \
@@ -37,7 +37,7 @@ cd sanity/monomer
 cp $scripts_dir/nomsa.slurm .; cp $scripts_dir/write_monomer.py .
 python write_monomer.py
 sbatch nomsa.slurm
-rm af2.err af2.out nomsa.slurm write_monomer.py
+rm nomsa.slurm write_monomer.py
 cd ../../
 
 # Complex
@@ -45,7 +45,15 @@ cd sanity/complex
 cp $scripts_dir/nomsa.slurm .; cp $scripts_dir/write_complex.py .; cp $scripts_dir/CD20.msa .
 python write_complex.py
 sbatch nomsa.slurm
-rm af2.err af2.out nomsa.slurm write_complex.py CD20.msa
+rm nomsa.slurm write_complex.py CD20.msa
+cd ../../
+
+# Linker
+cd sanity/full
+cp $scripts_dir/nomsa.slurm .; cp $scripts_dir/write_full.py .
+python write_full.py
+sbatch nomsa.slurm
+rm nomsa.slurm write_full.py
 cd ../../
 
 cd $scripts_dir
